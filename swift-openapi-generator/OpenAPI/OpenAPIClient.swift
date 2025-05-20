556b09575746199c6a21332bf3f178b88651b962
hamadayuuki <yuukirinnma@gmail.com>
2025-05-20 18:07:07 +0900

    Impl apiclient & struct using swift-openapi-generator

---
+++ swift-openapi-generator/OpenAPI/OpenAPIClient.swift
@@ -0,0 +1,37 @@
+//
+//  OpenAPIClient.swift
+//  swift-openapi-generator
+//
+//  Created by yuki.hamada on 2025/05/20.
+//
+
+import Foundation
+import OpenAPIRuntime
+import OpenAPIURLSession
+
+final class OpenAPIClient {
+    let client: Client
+    init() {
+        self.client = .init(
+            serverURL: try! Servers.server1(),
+            transport: URLSessionTransport()
+        )
+    }
+
+    func greet() async throws {
+        let response = try await client.getGreeting(
+            .init(query: .init(name: "hogehoge"))
+        )
+
+        switch response {
+        case .ok(let value):
+            switch value.body {
+//            case .json(let json):
+            case .json(let greeting):
+                let message = greeting.message
+            }
+        case .undocumented(statusCode: let statusCode, _):
+            print(statusCode)
+        }
+    }
+}
